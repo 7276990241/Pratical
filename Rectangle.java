13128f296784ae50d540bec71a6e1dcbb827d372
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-03-15 13:37:24 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,9 @@
+class Rectangle
+{
+public static void main(String args[])
+{
+int length=12,breadth=13,area;
+area=length*breadth;
+System.out.println("The sum Of number is:"+area);
+}
+}
---
+++ Rectangle_parameter.java
@@ -0,0 +1,9 @@
+class Rectangle_parameter
+{
+public static void main(String args[])
+{
+int length=12,breadth=13,POR;
+POR=2*(length*breadth);
+System.out.println("The sum Of number is:"+POR);
+}
+}
