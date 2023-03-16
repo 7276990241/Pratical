14ce8e3be197de451346d8aefcbe594e1e46d017
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-03-16 12:06:22 +0530

    Add files via upload

---
+++ Unary_op1.java
@@ -0,0 +1,15 @@
+class Unary_op1
+{
+public static void main(String args[])
+{
+int a=10;
+int b=10;
+System.out.println(a++);
+System.out.println(++a);
+System.out.println(a--);
+System.out.println(--a);
+System.out.println(a++ + ++b);
+System.out.println(++a + a++);
+System.out.println(b++ + ++a);
+}
+}
