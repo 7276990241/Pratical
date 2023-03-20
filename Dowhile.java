ce9d2ba758bc35220a5394503355a05619d0cc41
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-03-20 14:04:40 +0530

    Add files via upload

---
+++ Dowhile.java
@@ -0,0 +1,14 @@
+class Dowhile
+{
+public static void main(String[] args)
+{
+int i=0;
+System.out.println("Enter first 10 even numbers are:\n");
+do
+{
+System.out.println(i);
+i=i+2;
+}
+while(i<=10);
+}
+}
