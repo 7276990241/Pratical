335493440a49ddccf285fce2f7dd24ec80fbfd5e
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-03-20 14:03:58 +0530

    Add files via upload

---
+++ WhileloopEven.java
@@ -0,0 +1,13 @@
+class WhileloopEven
+{
+public static void main(String[] args)
+{
+int i=0;
+System.out.println("Enter first 10 even numbers are:\n");
+while(i<=10)
+{
+System.out.println(i);
+i=i+2;
+}
+}
+}
