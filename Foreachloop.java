8b8f4c394b9f51817cd20cdcfe56d856cd12dac0
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-03-20 14:03:13 +0530

    Add files via upload

---
+++ Foreachloop.java
@@ -0,0 +1,12 @@
+class Foreachloop
+{
+public static void main(String[] args)
+{
+String[] names = {"Pooja","Karishma","Priyanka","Nayan","Amit"};
+System.out.println("Printing the content of the student names:\n");
+for(String result:names)
+{
+System.out.println(result);
+}
+}
+}
