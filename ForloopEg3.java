e5648bc93f7dc120ea1147f027f0c06ee37ad8e5
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-03-20 14:02:50 +0530

    Add files via upload

---
+++ ForloopEg3.java
@@ -0,0 +1,13 @@
+class ForloopEg3
+{
+public static void main(String[] args)
+{
+int i;
+System.out.println("Enter first 10 numbers are:\n");
+for(i=1;i<=10;i++)
+{
+System.out.println(i);
+}
+System.out.println("\n");
+}
+}
