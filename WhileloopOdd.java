05ae38d672919e65aaa683d89b5909fe1cb3b690
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-03-20 14:04:20 +0530

    Add files via upload

---
+++ WhileloopOdd.java
@@ -0,0 +1,13 @@
+class WhileloopOdd
+{
+public static void main(String[] args)
+{
+int i=1;
+System.out.println("Enter first 10 odd numbers are:\n");
+while(i<=20)
+{
+System.out.println(i);
+i=i+2;
+}
+}
+}
