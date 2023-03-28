6a7f7f9bb3e27eb77c5fb7999068bb911204c22a
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-03-28 10:15:02 +0530

    Add files via upload

---
+++ Default_Eg2.java
@@ -0,0 +1,15 @@
+package obj;
+
+public class Default_Eg2 {
+	int S1_no;
+	String Name;
+	void result()
+	{
+		System.out.println("S1_no+"+Name);
+	}
+	public static void main(String []args)
+	{
+		Default_Eg2 obj = new Default_Eg2();
+		System.out.println("obj.s1_no+"+obj.Name);
+	}
+}
