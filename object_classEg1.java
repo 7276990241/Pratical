7037eca5105124d04c9d89d1149137396f21cfb4
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-03-28 10:13:51 +0530

    Add files via upload

---
+++ object_classEg1.java
@@ -0,0 +1,22 @@
+package com;
+
+public class object_classEg1 {
+	int S1_no=1;
+	int S2_no=2;
+	int S3_no=3;
+	String name="Priyanka 22 Solapur";
+	String namess="pqr  21 Mumbai";
+	String nam="abc 20 Pune";
+	public static void main(String[] args)
+	{
+		object_classEg1 obj= new object_classEg1();
+		System.out.println(obj.S1_no);
+		System.out.println(obj.name);
+		System.out.println(obj.S2_no);
+		System.out.println(obj.namess);
+		System.out.println(obj.S3_no);
+		System.out.println(obj.nam);
+	}
+
+
+}
