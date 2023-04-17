2c010216b234b5ebede9750ad63fb374a304a829
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-04-17 13:24:38 +0530

    Add files via upload

---
+++ ArrayEg2.java
@@ -0,0 +1,20 @@
+package array;
+// example of single dimensional array
+public class ArrayEg2
+{
+	public static void main(String[] args)
+	{
+		String [] names = {"Priyanka","Nayan","Karishma","Amit","Pruthvi","Pooja"};
+		for(int i=0;i<names.length;i++)
+		{
+			System.out.println(names[i]);
+		}
+		System.out.println();int [] phone = {123,456,789,955,333,555,111,2345,7890};
+		for (int j=0;j<phone.length;j++)
+		{
+			System.out.println(phone[j]);
+		}
+	}
+
+
+}
